478b96f0451b8f10329e649e6b685dd7d4180d65
Peter A. Fürstenau <peter_fuerstenau@hotmail.de>
2013-05-27 05:08:34 +0200

    "map.h" gegen Mehrfacheinbindung geschützt
    "map.h" definiert nun das Makro "MAP_H" um sich gegen Wiedereinbindung zu schützen. Außerdem wurden die Nicht-"typedef"-Bezeichner der Typen neben den "typedef"-Bezeichnern wiedereingeführt. Z.B. sollte nun zwischen "struct mapCell" und "mapCell" kein Unterschied bestehen.
    
    Die Funktion "pgenMapCell" wurde aus "map.h" entfernt, da sie dorthin niemals gehört hätte.

--- map.h
+++ map.h
@@ -1,19 +1,22 @@
-typedef struct {
+#ifndef MAP_H
+#define MAP_H
+
+typedef struct mapCell {
 	unsigned char walls[4], cntContent, x, y;
 	struct cellContent **contents;
 	struct map *map;
 } mapCell;
 
-typedef struct {
+typedef struct cellContent {
 	unsigned char type;
 	mapCell *cell;
 } cellContent;
 
-typedef struct {
+typedef struct map {
 	unsigned char height, width;
 	mapCell **cells;
 } map;
 
 map genMap(unsigned char height, unsigned char width, unsigned int randSeed);
 
-mapCell *pgenMapCell(unsigned char walls[4], unsigned char cntContent, unsigned char x, unsigned char y, cellContent **contents, map *map);
+#endif
